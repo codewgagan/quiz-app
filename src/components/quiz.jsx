8b37463c6fd9b8bd01ab79670689a3bad474cce2
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2025-08-17 21:43:45 +0530

    feat: initial commit - quiz app using React useState

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,17 +1,54 @@
+import React, { useState } from 'react';
+
 function Quiz(){
         const questionBank = [
-            {question: "What is the capital of India?"},
-            {options: ["Mumbai", "Delhi", "Bangalore"]},
-            {answer: "Delhi"}
-        ]
-    return <div>
+            {question: "What is the capital of India?",
+            options: ["Mumbai", "Delhi", "Bangalore"],
+            answer: "Delhi"
+        },
+        {
+            question: "Which is the scripting Language?",
+            options: ["Python", "JavaScript", "GoLang"],
+            answer: "JavaScript"
+        },
+        ];
 
-        <h2>Question</h2>
+        // using useState Hook
+        // var optionSelected = "None";
 
-        <div>{questionBank[0].question}</div>
-        <p>{questionBank[0].options}</p>
-        <button>{}</button>
+        const [optionSelected, setOptionSelected] = useState("None");
+
+
+        function handleSubmitOption (option){
+            console.log(option);
+            setOptionSelected(option);
+        }
+
+    return (
+    <div>
+        <h2>Question 1</h2> {/* Heading */}
+
+        {/* to show the question UI */}
+        <p className="question">{questionBank[0].question}</p>
+
+        {/* to show the question option UI */}
+        {questionBank[0].options.map((option)=>
+        <button className="option" key={option} onClick={()=>handleSubmitOption(option)}>
+        
+        {" "}
+        {option}{" "}
+        </button>
+        )}
+
+        {/* selecting and displaying selected option on screen */}
+        <p>option selected: {optionSelected}</p>
+
+        <div className="nav-buttons">
+            <button> Previous </button>
+            <button> Next </button>
         </div>
+    </div>
+    );
 }
 
 export default Quiz;
