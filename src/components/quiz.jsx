55249bea8d53dd8a7d1556c28de2aa0ec5a09123
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2025-08-18 23:27:33 +0530

    feat: implement quiz navigation, answer selection, and UI styling
    - Add quiz logic with navigation between questions and answer selection
    - Prevent moving to next question without selecting an answer
    - Style quiz and navigation buttons for improved

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -11,41 +11,70 @@ function Quiz(){
             options: ["Python", "JavaScript", "GoLang"],
             answer: "JavaScript"
         },
+        {
+            question: "What is the capital of USA?",
+            options: ["New York", "Washington DC", "Los Angeles"],
+            answer: "Washington DC"
+        },
         ];
 
         // using useState Hook
         // var optionSelected = "None";
 
-        const [optionSelected, setOptionSelected] = useState("None");
+        // const [optionSelected, setOptionSelected] = useState("None");  
+        const initialAnsewers = [null, null, null];
 
+        const [userAnswers, setUserAnswers] = useState(initialAnsewers);
 
-        function handleSubmitOption (option){
-            console.log(option);
-            setOptionSelected(option);
+        //represnt a current question using numbers
+        const [currentQuestion, setCurrentQuestion] = useState(0);
+
+        const selectedAnswer = userAnswers[currentQuestion];//null, option
+        function handleSelectOption (option){
+            // console.log(option);
+            // setOptionSelected(option);
+            //storing new answers of users used spread method
+            const userNewAnswers = [...userAnswers];
+            //that new answers will be for current question with selcted option
+            userNewAnswers[currentQuestion] = option;
+
+            //function 
+            setUserAnswers(userNewAnswers);
         }
 
+//function go to next
+function goToNext(){
+    setCurrentQuestion(currentQuestion + 1);
+}
+
+//function go to prev
+function goToPrev(){
+    if(currentQuestion>0){
+        setCurrentQuestion(currentQuestion - 1);
+    }
+}
     return (
     <div>
-        <h2>Question 1</h2> {/* Heading */}
+        <h2>Question {currentQuestion + 1 }</h2> {/* Heading */}
 
         {/* to show the question UI */}
-        <p className="question">{questionBank[0].question}</p>
+        <p className="question">{questionBank[currentQuestion].question}</p>
 
         {/* to show the question option UI */}
-        {questionBank[0].options.map((option)=>
-        <button className="option" key={option} onClick={()=>handleSubmitOption(option)}>
+        {questionBank[currentQuestion].options.map((option)=>
+        <button className={"option" + (selectedAnswer === option ? " selected" : "")} key={option} onClick={()=>handleSelectOption(option)}>
         
-        {" "}
-        {option}{" "}
+        {option}
         </button>
         )}
 
         {/* selecting and displaying selected option on screen */}
-        <p>option selected: {optionSelected}</p>
+        {/* <p>option selected: {optionSelected}</p> */}
 
         <div className="nav-buttons">
-            <button> Previous </button>
-            <button> Next </button>
+            <button onClick={goToPrev} disabled={currentQuestion === 0}> Previous </button>
+            <button onClick={goToNext} disabled={!selectedAnswer}> {currentQuestion === questionBank.length -1 ? "Finish Quiz" : "Next"}</button>
+            
         </div>
     </div>
     );
